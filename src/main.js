68446f4a4c9f5158363c1bf892ae7d40d75325fd
JupSat <jupsat@163.com>
2023-04-10 12:23:29 +0800

    feat: 配置styles

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/styles/index.scss'
 import start from '@/micros'
 import * as Icons from '@element-plus/icons-vue'
 
