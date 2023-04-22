bc734b697596a9d0e3fce699528d11f49ef0df00
JupSat <jupsat@163.com>
2023-04-22 15:52:22 +0800

    feat: 添加国际化

---
+++ src/language/index.js
@@ -0,0 +1,18 @@
+import { createI18n } from 'vue-i18n'
+import ZH from './common/zh'
+import EN from './common/en'
+
+import summaryZh from './summary/zh'
+import summaryEn from './summary/en'
+const messages = {
+  zh: { ...ZH, ...summaryZh },
+  en: { ...EN, ...summaryEn },
+}
+
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: 'zh',
+  messages,
+})
+export default i18n
