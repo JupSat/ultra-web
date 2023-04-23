0b4957dc1172df39e8d582f409fa63f92ec3b375
JupSat <jupsat@163.com>
2023-04-23 18:11:35 +0800

    feat: 添加请求方法

---
+++ src/utils/http/function.js
@@ -0,0 +1,54 @@
+/*
+ * @Description: 接口基础请求方法
+ * @version:
+ * @Author: JupSat
+ * @email: jupsat@163.com
+ * @Date: 2023-04-23 18:09:10
+ * @LastEditors: JupSat
+ * @LastEditTime: 2023-04-23 18:09:14
+ */
+import request from '@/utils/http/request'
+
+export const rGet = (url, data = {}, params = {}) => {
+  return request({
+    method: 'get',
+    url,
+    data,
+    params,
+  })
+}
+
+export const rPost = (url, data = {}, params = {}) => {
+  return request({
+    method: 'post',
+    url,
+    data,
+    params,
+  })
+}
+
+export const rGetBlob = (url, data) => {
+  return request({
+    method: 'get',
+    url,
+    responseType: 'blob',
+    data,
+  })
+}
+
+export const rPut = (url, data) => {
+  return request({
+    method: 'put',
+    url,
+    data,
+  })
+}
+
+export const rDelete = (url, data = {}, params = {}) => {
+  return request({
+    method: 'delete',
+    url,
+    data,
+    params,
+  })
+}
