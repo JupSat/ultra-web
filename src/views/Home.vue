06fbc4e0ea3d2f36804a2c04107048f516fc247e
JupSat <jupsat@163.com>
2023-04-26 16:28:51 +0800

    feat: hooks使用例子

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
  * @email: jupsat@163.com
  * @Date: 2023-03-21 16:11:32
  * @LastEditors: JupSat
- * @LastEditTime: 2023-04-18 16:47:51
+ * @LastEditTime: 2023-04-26 16:28:01
 -->
 <template>
   <div class="app-home">
@@ -17,7 +17,15 @@
       <el-tab-pane label="Example2" name="second"
         ><Example2></Example2
       ></el-tab-pane>
-      <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+      <el-tab-pane label="Hooks" name="third">
+        <div>
+          num1:<input v-model.number="num1" style="width: 100px" />
+          <br />
+          num2:<input v-model.number="num2" style="width: 100px" />
+        </div>
+        <span>加法等于:{{ addNum }}</span> <br />
+        <span>减法等于:{{ subNum }}</span>
+      </el-tab-pane>
       <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
     </el-tabs>
   </div>
@@ -34,6 +42,17 @@ import { nextTick, ref } from 'vue'
 // import HelloWorld from '@/components/HelloWorld.vue'
 import Example from './Example'
 import Example2 from './Example2'
+import useAdd from '@/hooks/useAdd.js' //引入自动hook
+import { useSub } from '@/hooks/useSub.js' //引入自动hook
+
+const num1 = ref(2)
+const num2 = ref(1)
+//加法功能-自定义Hook（将响应式变量或者方法形式暴露出来）
+const { addNum, addFn } = useAdd({ num1, num2 })
+addFn(num1.value, num2.value)
+//减法功能-自定义Hook (将响应式变量或者方法形式暴露出来)
+const { subNum, subFn } = useSub({ num1, num2 })
+subFn(num1.value, num2.value)
 
 const activeName = ref('first')
 
